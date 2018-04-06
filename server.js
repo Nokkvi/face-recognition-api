4a0ff7421637539d8685376901b1aab3685a8ce3
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-04-06 19:11:16 +0000

    Added default port if PORT is not found

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/image', (req, res) => {image.handleApiCall(req, res)})
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
 	console.log(`app is running on port ${PORT}`);
 });
 
