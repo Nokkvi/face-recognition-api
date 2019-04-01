fbfeb971148e82d8eb32de04eceafd3881dae80b
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2019-04-01 22:20:59 +0000

    Added a profile update endpoint

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db ,bcryp
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
+app.put('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)})
+
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/image', (req, res) => {image.handleApiCall(req, res)})
